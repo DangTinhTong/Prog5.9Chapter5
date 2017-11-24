a0fbc809746b5b542c11e536b042fd783cfe44ec
admin <dangtinhtong@gmail.com>
2017-11-24 23:57:34 +0700

    Initial Commit

---
+++ prog5.9/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog5.9
+//
+//  Created by admin on 11/24/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
